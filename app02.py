cbfb8599d4ae037d96ab5cc2a481baad09aba129
ibounnet <159422323+ibounnet@users.noreply.github.com>
2024-02-13 16:44:54 +0700

    Add files via upload

---
+++ app02.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.sidebar.text("Sidebar")
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.title('OOP in ML')
+st.image('io.jpg')
+st.text_input("What is your name")
