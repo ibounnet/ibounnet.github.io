91eadbe87cc99f438f45c96853aa89878ce8545b
ibounnet <warangkhana.ja.66@ubu.ac.th>
2024-02-16 14:54:01 +0700

    add

--- streamlit/app02.py
+++ streamlit/app02.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.sidebar.text("Sidebar")
-st.sidebar.button('go>')
-st.sidebar.button('went>')
-st.sidebar.button('gone>')
+st.sidebar.button('libarly')
+st.sidebar.button('corse')
+st.sidebar.button('BMI')
 st.title('OOP in ML')
 st.image('io.jpg')
 st.text_input("What is your name")
