846a3f2c9a61f9ec412e09cece2998eb762f1ba3
ibounnet <159422323+ibounnet@users.noreply.github.com>
2024-02-20 13:46:03 +0700

    Delete streamlit directory

--- streamlit/app02.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.sidebar.text("Sidebar")
-st.sidebar.button('libarly')
-st.sidebar.button('corse')
-st.sidebar.button('BMI')
-st.title('OOP in ML')
-st.image('io.jpg')
-st.text_input("What is your name")
